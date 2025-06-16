d4424e84fe466e31ca68412e13633973fee19490
Sagar Manchakatla <sagarmanchakatla01@gmail.com>
2025-06-16 10:46:36 +0530

    trying portkey

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 // Load up env file which contains credentials
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+// dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+dotenv.config({ path: ".env" });
 
 import { Server } from "./server";
 
 const server = new Server();
-server.listen(8080);
+server.listen(8000);
